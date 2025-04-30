948a00285d1f521b014061f642b30757152019a5
Annas Abdurrahman <68950058+annasabdurrahman354@users.noreply.github.com>
2025-04-30 11:43:29 +0700

    feat: command web

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,8 +32,35 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('sanna'),
             'email_verified_at' => now(),
         ]);
+        $user1 = User::create([
+            'nama' => 'Ardho Masiech Firdaus',
+            'nama_panggilan' => 'Ardho',
+            'jenis_kelamin' => 'laki-laki',
+            'nomor_telepon' => '0895708149200',
+            'email' => 'ardhomasfir@gmail.com',
+            'role' => UserRole::SANTRI->value,
+            'status' => UsersStatus::AKTIF->value,
+            'password' => Hash::make('ohdra'),
+            'email_verified_at' => now(),
+        ]);
+
+        $user2 = User::create([
+            'nama' => 'Diaz Seno Hutomo',
+            'nama_panggilan' => 'Diaz',
+            'jenis_kelamin' => 'laki-laki',
+            'nomor_telepon' => '0895322208791',
+            'email' => 'diazsenohutomo2010@gmail.com',
+            'role' => UserRole::SANTRI->value,
+            'status' => UsersStatus::LULUS->value,
+            'password' => Hash::make('didik'),
+            'email_verified_at' => now(),
+            ]);
+
 
         Artisan::call('shield:super-admin', ['--user' => $user->id]);
+        Artisan::call('shield:super-admin', ['--user' => $user1->id]);
+        Artisan::call('shield:super-admin', ['--user' => $user2->id]);
+
         $this->call([
             InitialSeeder::class,
         ]);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Inertia\PendaftaranController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,4 +13,18 @@ Route::get('/pendaftaran/create', \App\Livewire\Pendaftaran\PendaftaranCreate::c
 Route::get('/pendaftaran/', [PendaftaranController::class, 'index'])->name('pendaftaran.index');
 Route::get('/pendaftaran/finish/{id}', [PendaftaranController::class, 'finish'])->name('pendaftaran.finish');
 
+Route::get('/command/storage/link', function () {
+    Artisan::call('storage:link');
+    dd("Storage is linked");
+});
+
+Route::get('/command/filament/optimize', function () {
+    Artisan::call('filament:optimize');
+    dd("Filament is optimized");
+});
+
+Route::get('/command/filament/optimize-clear', function () {
+    Artisan::call('filament:optimize-clear');
+    dd("Filament cache is cleared");
+});
 
